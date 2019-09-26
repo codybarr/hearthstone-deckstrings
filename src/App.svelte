67911a433f86b0e174ac8d45f608a80ad02ad8f6
Cody Barr <cbarr@ministrysync.com>
2019-09-26 14:34:18 -0400

    Added card quantities. Updated dependencies

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,34 @@
 <script>
-	import DeckList from './components/DeckList.svelte'
-	import HearthstoneIcon from './components/HearthstoneIcon.svelte'
-	import axios from 'axios'
+  import DeckList from "./components/DeckList.svelte";
+  import HearthstoneIcon from "./components/HearthstoneIcon.svelte";
+  import axios from "axios";
 
-	// import deck from './deck' // for testing
-	let deck = ''
-	let deckstring = ''
-	let loading = false
-	let error = ''
+  // import deck from './deck' // for testing
+  let deck = "";
+  let deckstring = "";
+  let loading = false;
+  let error = "";
 
-	console.log(
-		'Howdy, thanks for visiting. Try the following deckstring for an example:'
-	)
-	console.log(
-		'AAECAa0GAqUJqaUDDvsB5QSIBfYH0QrWCvIM+wzQ/gKXhwOClAOYmwOvpQOfqQMA'
-	)
+  console.log(
+    "Howdy, thanks for visiting. Try the following deckstring for an example:"
+  );
+  console.log(
+    "AAECAa0GAqUJqaUDDvsB5QSIBfYH0QrWCvIM+wzQ/gKXhwOClAOYmwOvpQOfqQMA"
+  );
 
-	async function submit() {
-		deck = ''
-		error = ''
-		loading = true
-		try {
-			const response = await axios.post('/api/deckstring', { deckstring })
-			deck = response.data
-		} catch (err) {
-			error = `🤔 Hm...something's not quite right with that deckstring. Try again.`
-		} finally {
-			loading = false
-		}
-	}
+  async function submit() {
+    deck = "";
+    error = "";
+    loading = true;
+    try {
+      const response = await axios.post("/api/deckstring", { deckstring });
+      deck = response.data;
+    } catch (err) {
+      error = `🤔 Hm...something's not quite right with that deckstring. Try again.`;
+    } finally {
+      loading = false;
+    }
+  }
 </script>
 
 <style lang="postcss">
@@ -36,41 +36,41 @@
 </style>
 
 <div
-	class="px-5 py-4 bg-gray-800 text-white border-b border-1 border-gray-300
-	relative flex items-center">
-	<h1 class="text-xl">Deckstrings</h1>
-	<div class="flex flex-stretch absolute right-0">
-		<input
-			class="bg-gray-200 text-base text-gray-700 border border-gray-300 rounded
-			px-3 mr-5 leading-tight focus:outline-none focus:bg-white w-96"
-			type="text"
-			aria-label="Enter deckstring"
-			placeholder="enter deckstring"
-			bind:value={deckstring} />
-		<button
-			class="px-5 py-2 bg-blue-700 text-white text-base mr-5 rounded
-			hover:bg-blue-300 hover:text-gray-900"
-			on:click|preventDefault={submit}>
-			Submit
-		</button>
-	</div>
+  class="px-5 py-4 bg-gray-800 text-white border-b border-1 border-gray-300
+  relative flex items-center">
+  <h1 class="text-xl">Deckstrings</h1>
+  <div class="flex flex-stretch absolute right-0">
+    <input
+      class="bg-gray-200 text-base text-gray-700 border border-gray-300 rounded
+      px-3 mr-5 leading-tight focus:outline-none focus:bg-white w-96"
+      type="text"
+      aria-label="Enter deckstring"
+      placeholder="enter deckstring"
+      bind:value={deckstring} />
+    <button
+      class="px-5 py-2 bg-blue-700 text-white text-base mr-5 rounded
+      hover:bg-blue-300 hover:text-gray-900"
+      on:click|preventDefault={submit}>
+      Submit
+    </button>
+  </div>
 </div>
 
 <div class="p-5">
-	{#if loading}
-		<p>Loading...</p>
-	{/if}
+  {#if loading}
+    <p>Loading...</p>
+  {/if}
 
-	{#if error}
-		<p>{error}</p>
-	{/if}
+  {#if error}
+    <p>{error}</p>
+  {/if}
 
-	{#if deck !== ''}
-		<div class="text-center">
-			<HearthstoneIcon
-				name={deck.hero.cardClass.toLowerCase()}
-				classes="inline-block w-20 h-20 fill-current text-black mb-4 mx-auto" />
-		</div>
-		<DeckList {deck} />
-	{/if}
+  {#if deck !== ''}
+    <div class="text-center">
+      <HearthstoneIcon
+        name={deck.hero.cardClass.toLowerCase()}
+        classes="inline-block w-20 h-20 fill-current text-black mb-4 mx-auto" />
+    </div>
+    <DeckList {deck} />
+  {/if}
 </div>
--- src/components/CardRow.svelte
+++ src/components/CardRow.svelte
@@ -1,96 +1,97 @@
 <script>
-	import HearthstoneIcon from './HearthstoneIcon.svelte'
-	export let card
+  import HearthstoneIcon from "./HearthstoneIcon.svelte";
+  export let card;
 
-	let showImage = false
-	let loaded = false
+  let showImage = false;
+  let loaded = false;
 
-	let pos = { x: 0, y: 0 }
+  let pos = { x: 0, y: 0 };
 
-	let rarityColors = {
-		FREE: 'text-black',
-		COMMON: 'text-yellow-500',
-		RARE: 'text-blue-400',
-		EPIC: 'text-purple-600',
-		LEGENDARY: 'text-orange-600'
-	}
+  let rarityColors = {
+    FREE: "text-black",
+    COMMON: "text-yellow-500",
+    RARE: "text-blue-400",
+    EPIC: "text-purple-600",
+    LEGENDARY: "text-orange-600"
+  };
 
-	$: colorClass = rarityColors[card.rarity]
+  $: colorClass = rarityColors[card.rarity];
 </script>
 
 <style lang="postcss">
-	a:hover {
-		@apply underline;
-	}
+  a:hover {
+    @apply underline;
+  }
 
-	li {
-		@apply my-2;
-	}
+  li {
+    @apply my-2;
+  }
 </style>
 
 <li class="font-bold flex flex-row items-center max-w-xs">
-	<div class="flex-1 pr-3">
-		<a
-			on:mouseover={() => {
-				showImage = true
-				loaded = true
-			}}
-			on:mouseout={() => (showImage = false)}
-			on:mousemove={event => {
-				pos.x = event.clientX
-				pos.y = event.clientY
-			}}
-			class="{colorClass} flex-1 pr-3"
-			href="https://s3.wasabisys.com/hearthstone/{card.dbfId}.png"
-			alt="this is a link"
-			target="_blank">
-			{card.name}
-		</a>
-	</div>
-	<span class="flex items-center">
-		<span class="pr-1">{card.cost}</span>
-		<HearthstoneIcon
-			name="mana-crystal"
-			classes="inline-block w-4 h-4 fill-current text-indigo-600" />
-	</span>
+  <div class="flex-1 pr-3">
+    <span>{card.quantity}x&nbsp;</span>
+    <a
+      on:mouseover={() => {
+        showImage = true;
+        loaded = true;
+      }}
+      on:mouseout={() => (showImage = false)}
+      on:mousemove={event => {
+        pos.x = event.clientX;
+        pos.y = event.clientY;
+      }}
+      class="{colorClass} flex-1 pr-3"
+      href="https://s3.wasabisys.com/hearthstone/{card.dbfId}.png"
+      alt="this is a link"
+      target="_blank">
+      {card.name}
+    </a>
+  </div>
+  <span class="flex items-center">
+    <span class="pr-1">{card.cost}</span>
+    <HearthstoneIcon
+      name="mana-crystal"
+      classes="inline-block w-4 h-4 fill-current text-indigo-600" />
+  </span>
 
-	{#if loaded}
-		<div
-			class="absolute"
-			class:invisible={!showImage}
-			style="top: {pos.y - 32}px; left: {pos.x + 93}px">
-			<svg
-				xmlns:svg="http://www.w3.org/2000/svg"
-				width="64px"
-				height="64px"
-				viewBox="0 0 128 128"
-				xml:space="preserve">
-				<g>
-					<path
-						d="M75.4 126.63a11.43 11.43 0 0 1-2.1-22.65 40.9 40.9 0 0 0
-						30.5-30.6 11.4 11.4 0 1 1 22.27 4.87h.02a63.77 63.77 0 0 1-47.8
-						48.05v-.02a11.38 11.38 0 0 1-2.93.37z"
-						fill="#000000"
-						fill-opacity="1" />
-					<animateTransform
-						attributeName="transform"
-						type="rotate"
-						from="0 64 64"
-						to="360 64 64"
-						dur="1200ms"
-						repeatCount="indefinite" />
-				</g>
-			</svg>
-		</div>
-		<div
-			class="absolute"
-			class:invisible={!showImage}
-			style="top: {pos.y - 170}px; left: {pos.x + 20}px">
-			<!-- <p>X: {pos.x}, Y: {pos.y}</p> -->
-			<img
-				width="250px"
-				src="https://s3.wasabisys.com/hearthstone/{card.dbfId}.png"
-				alt={card.name} />
-		</div>
-	{/if}
+  {#if loaded}
+    <div
+      class="absolute"
+      class:invisible={!showImage}
+      style="top: {pos.y - 32}px; left: {pos.x + 93}px">
+      <svg
+        xmlns:svg="http://www.w3.org/2000/svg"
+        width="64px"
+        height="64px"
+        viewBox="0 0 128 128"
+        xml:space="preserve">
+        <g>
+          <path
+            d="M75.4 126.63a11.43 11.43 0 0 1-2.1-22.65 40.9 40.9 0 0 0
+            30.5-30.6 11.4 11.4 0 1 1 22.27 4.87h.02a63.77 63.77 0 0 1-47.8
+            48.05v-.02a11.38 11.38 0 0 1-2.93.37z"
+            fill="#000000"
+            fill-opacity="1" />
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            from="0 64 64"
+            to="360 64 64"
+            dur="1200ms"
+            repeatCount="indefinite" />
+        </g>
+      </svg>
+    </div>
+    <div
+      class="absolute"
+      class:invisible={!showImage}
+      style="top: {pos.y - 170}px; left: {pos.x + 20}px">
+      <!-- <p>X: {pos.x}, Y: {pos.y}</p> -->
+      <img
+        width="250px"
+        src="https://s3.wasabisys.com/hearthstone/{card.dbfId}.png"
+        alt={card.name} />
+    </div>
+  {/if}
 </li>
